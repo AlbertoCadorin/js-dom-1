0b847e943857da8ce28377f98bda0cab20f6871b
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-03-04 17:35:33 +0100

    feat: add fuction to turn on the light bulb

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-
+// riciamare il bottone tramite id
+const elementClick = document.getElementById("butt");
+console.log(elementClick);
+// richiamare l'img tramite selettore 
+const elementImg = document.querySelector('.lampadina')
+console.log(elementImg)
+// cambiare l'img con la lampadina accesa
+elementClick.addEventListener('click', function () {
+    elementImg.src = 'img/yellow_lamp.png';
+    elementImg.alt = 'Meme di Gaetano documentazione';
+})
