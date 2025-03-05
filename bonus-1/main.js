da2c9fa234a6f7073ad9370702f4bf5530b8ee1f
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-03-05 10:35:07 +0100

    feat: add exercise bonus

--- bonus-1/main.js
+++ bonus-1/main.js
@@ -1,11 +1,25 @@
 // riciamare il bottone tramite id
 const elementClick = document.getElementById("butt");
 console.log(elementClick);
+
 // richiamare l'img tramite selettore 
-const elementImg = document.querySelector('.lampadina')
+const elementImg = document.getElementById('lampadina')
 console.log(elementImg)
+
 // cambiare l'img con la lampadina accesa
-elementClick.addEventListener('click', function () {
-    elementImg.src = '../img/yellow_lamp.png';
-    elementImg.alt = 'Meme di Gaetano documentazione';
+elementClick.addEventListener('click', function(){
+    if(elementImg.classList.contains('off')){
+        console.log('accendo ')
+        elementImg.classList.remove('off')
+        elementImg.src = '../img/yellow_lamp.png';
+        elementImg.alt = 'Meme di Gaetano documentazione';
+        elementClick.innerText = 'Spegni!';
+    }else{
+        console.log('spengo ')
+        elementImg.classList.add('off')
+        elementImg.src = '../img/white_lamp.png';
+        elementImg.alt = 'lampadina spenta';
+        elementClick.innerText = 'Accendi!';
+    }
+    
 })
